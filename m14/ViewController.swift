b0e073b3149361bd0cb54700b34b223f971071be
ti1ek <tilek.koszhanov@mail.ru>
2023-03-25 13:10:23 +0500

    Initial Commit

---
+++ m14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m14
+//
+//  Created by Zhuldyz Bukeshova on 25.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
